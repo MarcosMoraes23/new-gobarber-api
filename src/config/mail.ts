1f693e3f3ae5ab41327e9698641a6ed08bfa1720
Marcos Moraes <marcosmoraes2303@gmail.com>
2020-06-30 23:23:48 -0300

    Amazon SES configurations

---
+++ src/config/mail.ts
@@ -0,0 +1,22 @@
+import { string } from '@hapi/joi';
+
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'marcosmoraes2303@gmail.com',
+      name: 'Marcos Moraes',
+    },
+  },
+} as IMailConfig;
